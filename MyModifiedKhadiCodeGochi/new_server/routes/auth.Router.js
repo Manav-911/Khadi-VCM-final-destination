0d4e816e977463a66590b70f2f1e10e61b5aed56
HarryPotter1317 <darjeeprachi5@gmail.com>
2025-08-11 14:30:28 +0530

    cheeze udai

--- MyModifiedKhadiCodeGochi/new_server/routes/auth.Router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { userLogin, adminLogin } = require('../controllers/auth.controller.js');
-
-const authRouter = express.Router();
-
-authRouter.post("/user", userLogin);
-authRouter.post("/admin", adminLogin)
-
-module.exports = authRouter;
